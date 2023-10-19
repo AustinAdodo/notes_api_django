2814f1fef7c9b1d843cb6481d33ed77b625e263d
Odia Adodo <subzbelow@gmail.com>
2023-10-19 16:49:49 +0100

    added models.py

---
+++ Notes_api_django/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Note(models.Model):
+    objects = None
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default="")
+    text = models.TextField()
+    owner = models.ForeignKey("auth.User", related_name="notes", on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = "created",
