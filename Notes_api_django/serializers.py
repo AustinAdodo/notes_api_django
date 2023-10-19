792850eb58b5414d29b2c97f04b90f72c616a9e8
Odia Adodo <subzbelow@gmail.com>
2023-10-19 16:52:33 +0100

    added serializers.py

---
+++ Notes_api_django/serializers.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from models import Note
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+
+    class Meta:
+        model = Note
+        fields = "id", "title", "text", "owner",
+
+
+class UserSerializer(serializers.ModelSerializer):
+    notes = serializers.PrimaryKeyRelatedField(many=True, queryset=Note.objects.all())
+
+    class Meta:
+        model = User
+        fields = "id", "username", "notes",
