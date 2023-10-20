b6e898268e88b6a30612dcf3071325883d1ed5b9
Odia Adodo <subzbelow@gmail.com>
2023-10-20 21:06:24 +0100

    configured swagger UI

--- Notes_api_django/__init__.py
+++ Notes_api_django/__init__.py
@@ -1,3 +1,5 @@
 # python manage.py runserver
 # pip install djangorestframework
 # pip install drf-yasg
+# python manage.py runserver
+# http://localhost:8000/swagger/
--- Notes_api_django/urls.py
+++ Notes_api_django/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.urls import path, include
+from rest_framework import permissions
 from rest_framework.urlpatterns import format_suffix_patterns
 from Notes_api_django import views
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+# Customize the Swagger schema in get_schema_view()
 schema_view = get_schema_view(
     openapi.Info(
         title="Your API",
@@ -51,7 +53,7 @@ urlpatterns = format_suffix_patterns(urlpatterns)
 urlpatterns += [
     path("api-auth/", include("rest_framework.urls")),
     path('admin/', admin.site.urls),
-re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
             name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
          name='schema-swagger-ui'),
